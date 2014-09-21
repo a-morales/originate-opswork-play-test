83c55d666ff878099fdd64561065b1ff77d2882d
Antonio Morales <Antonio.morales1990@gmail.com>
2014-09-21 11:11:31 -0700

    setup chef deployment files

---
+++ app/metadata.rb
@@ -0,0 +1,10 @@
+name             'app'
+maintainer       'Antonio Morales'
+maintainer_email 'antonio.morales@originate.com'
+license          'All rights reserved'
+description      'Installs/Configures app'
+long_description 'Installs/Configures app'
+version          '0.1.0'
+
+depends "play2", "~> 0.1.0" # add a dependnecy on Originate's play2 cookbook
+
